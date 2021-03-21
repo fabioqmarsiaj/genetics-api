d70ec904760aa78f72356fb4801fb2889fa2ceba
Fabio <fabiomarsiaj@gmail.com>
2021-03-20 21:01:58 -0300

    first commit

---
+++ src/test/java/com/fabiomarsiaj/genetics/api/GeneticsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fabiomarsiaj.genetics.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeneticsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
